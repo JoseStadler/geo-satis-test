e5c7b9593c0c78ff6c6a3eda2a012295547c9469
Jose Stadler <josestadler52@gmail.com>
2022-05-17 16:53:22 -0500

    code cleaning

--- geo-satis-web/src/app/modules/offenders/offenders.component.ts
+++ geo-satis-web/src/app/modules/offenders/offenders.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { of, tap } from 'rxjs';
 import { Offender } from './models/offender.model';
 import { OffendersActions } from './store/action-types';
 import {
